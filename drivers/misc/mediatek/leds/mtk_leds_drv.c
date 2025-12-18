992a1598be01ee45dc36ed0f77e8d6d86ba1b685
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2025-12-18 20:23:46 +0900

    Update mtk_leds_drv.c

--- drivers/misc/mediatek/leds/mtk_leds_drv.c
+++ drivers/misc/mediatek/leds/mtk_leds_drv.c
@@ -22,8 +22,8 @@
 #include <linux/wakelock.h>
 #include <linux/slab.h>
 #include <linux/spinlock.h>
-#include <mtk_leds_hal.h>
-#include <mtk_leds_drv.h>
+#include <../mt8167/mtk_leds_hal.h>
+#include <../mt8167/mtk_leds_drv.h>
 #ifdef CONFIG_MTK_PWM
 #include <mt-plat/mtk_pwm.h>
 #endif
