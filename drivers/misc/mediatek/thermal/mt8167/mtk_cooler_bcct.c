a06c82a08b9a25657727e87ee8c9202a7252cb65
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2025-12-18 22:14:15 +0900

    Update mtk_cooler_bcct.c

--- drivers/misc/mediatek/thermal/mt8167/mtk_cooler_bcct.c
+++ drivers/misc/mediatek/thermal/mt8167/mtk_cooler_bcct.c
@@ -23,7 +23,7 @@
 #include <linux/syscalls.h>
 #include "mt-plat/mtk_thermal_monitor.h"
 #include <charging.h>
-#include <inc/tmp_battery.h>
+#include <../../../../../drivers/misc/mediatek/thermal/mt8167/inc/tmp_battery.h>
 #include <linux/uidgid.h>
 #include <linux/notifier.h>
 #include <linux/fb.h>
