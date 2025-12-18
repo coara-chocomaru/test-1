b3cdf6a28ea050cbb37a9a15939e903e7801943b
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2025-12-18 20:24:17 +0900

    Update mtk_leds_drv.h

--- drivers/misc/mediatek/leds/mtk_leds_drv.h
+++ drivers/misc/mediatek/leds/mtk_leds_drv.h
@@ -12,7 +12,7 @@
  */
 
 #include <linux/leds.h>
-#include <mtk_leds_hal.h>
+#include <../mt8167/mtk_leds_hal.h>
 
 /****************************************************************************
  * LED DRV functions
